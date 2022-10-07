f9d68839a3c27d523c76a98b9e750319417c879e
leonw774 <leonw774@gmail.com>
2022-10-07 15:33:13 +0800

    Update experiment_on_bpe_parameters.sh

--- experiment_on_bpe_parameters.sh
+++ experiment_on_bpe_parameters.sh
@@ -6,20 +6,20 @@
 
 # experiment on merge condition
 ./pipeline.sh lmd_full_posevent ours_sample1.0 no_train --use-existed
-./pipeline.sh symphonynet_posevent ours_sample1.0 no_train --use-existed
 ./pipeline.sh lmd_full_posevent musicbpe_sample1.0 no_train --use-existed
+./pipeline.sh symphonynet_posevent ours_sample1.0 no_train --use-existed
 ./pipeline.sh symphonynet_posevent musicbpe_sample1.0 no_train --use-existed
 
 # experiment on sample rate
 ./pipeline.sh lmd_full_posevent ours_sample1.0 no_train --use-existed
-./pipeline.sh symphonynet_posevent ours_sample1.0 no_train --use-existed
 ./pipeline.sh lmd_full_posevent ours_sample0.1 no_train --use-existed
+./pipeline.sh symphonynet_posevent ours_sample1.0 no_train --use-existed
 ./pipeline.sh symphonynet_posevent ours_sample0.1 no_train --use-existed
 
 # experiment on scoring method on
 ./pipeline.sh lmd_full_posevent ours_wplike_sample1.0 no_train --use-existed
-./pipeline.sh symphonynet_posevent ours_wplike_sample1.0 no_train --use-existed
 ./pipeline.sh lmd_full_posevent ours_wplike_sample0.1 no_train --use-existed
+./pipeline.sh symphonynet_posevent ours_wplike_sample1.0 no_train --use-existed
 ./pipeline.sh symphonynet_posevent ours_wplike_sample0.1 no_train --use-existed
 
 # experiment on applying vocabulary from corpus A to corpus B
