253be753c984af38a9e80438c8a6106534a5329d
leonw774 <leonw774@gmail.com>
2022-10-19 23:24:19 +0800

    Update experiment_on_bpe_parameters.sh

--- experiment_on_bpe_parameters.sh
+++ experiment_on_bpe_parameters.sh
@@ -6,9 +6,9 @@
 
 # experiment on merge condition
 ./pipeline.sh lmd_full_posevent ours_sample1.0 no_train --use-existed
-./pipeline.sh lmd_full_posevent musicbpe_sample1.0 no_train --use-existed
+./pipeline.sh lmd_full_posevent mulpi_sample1.0 no_train --use-existed
 ./pipeline.sh snd_posevent ours_sample1.0 no_train --use-existed
-./pipeline.sh snd_posevent musicbpe_sample1.0 no_train --use-existed
+./pipeline.sh snd_posevent mulpi_sample1.0 no_train --use-existed
 
 # experiment on sample rate
 ./pipeline.sh lmd_full_posevent ours_sample1.0 no_train --use-existed
@@ -16,7 +16,7 @@
 ./pipeline.sh snd_posevent ours_sample1.0 no_train --use-existed
 ./pipeline.sh snd_posevent ours_sample0.1 no_train --use-existed
 
-# experiment on scoring method on
+# experiment on scoring method
 ./pipeline.sh lmd_full_posevent ours_wplike_sample1.0 no_train --use-existed
 ./pipeline.sh lmd_full_posevent ours_wplike_sample0.1 no_train --use-existed
 ./pipeline.sh snd_posevent ours_wplike_sample1.0 no_train --use-existed
