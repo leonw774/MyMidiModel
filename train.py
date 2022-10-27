cc3e4e30c31a7a8a1cef20d1b9f770dcc0978453
leonw774 <leonw774@gmail.com>
2022-10-27 18:11:22 +0800

    Update train.py

--- train.py
+++ train.py
@@ -285,7 +285,7 @@ def main():
     logging.info(args_str)
 
     if args.use_device == 'cuda':
-        logging.info('Using CUDA deivces: %s', torch.cuda.current_device())
+        logging.info('Torch sees %d CUDA devices. Using deivces #%d', torch.cuda.device_count(), torch.cuda.current_device())
 
     # loss csv file is in the checkpoint directory
     loss_file_path = os.path.join(args.model_dir_path, 'loss.csv')
