942f4f36d870a18b8169260202986952975bcfef
leonw774 <leonw774@gmail.com>
2022-10-27 18:03:53 +0800

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -142,7 +142,7 @@ if [ -f "./set_cuda_device_env_var.sh" ]; then
     source ./set_cuda_device_env_var.sh
 fi
 # CUDA_VISIBLE_DEVICES=0,1
-$SET_CUDA_DEVICE_ENV_VAR python3 train.py --max-seq-length $MAX_SEQ_LENGTH $TRAIN_OTHER_ARGUMENTS \
+python3 train.py --max-seq-length $MAX_SEQ_LENGTH $TRAIN_OTHER_ARGUMENTS \
     --layers-number $LAYERS_NUMBER --attn-heads-number $ATTN_HEADS_NUMBER --embedding-dim $EMBEDDING_DIM \
     --split-ratio $SPLIT_RATIO --batch-size $BATCH_SIZE --steps $STEPS --validation-interval $VALIDATION_INTERVAL --grad-norm-clip $GRAD_NORM_CLIP --early-stop-tolerance $EARLY_STOP_TOLERANCE \
     --lr $LEARNING_RATE --lr-warmup-steps $LEARNING_RATE_WARMUP_STEPS --lr-decay-end-steps $LEARNING_RATE_DECAY_END_STEPS --lr-decay-end-ratio $LEARNING_RATE_DECAY_END_RATIO \
