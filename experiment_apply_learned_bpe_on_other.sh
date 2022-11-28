5f3731444e1e41286ea7f2aefc91106fdc5fc324
leonw774 <leonw774@gmail.com>
2022-11-28 18:15:34 +0800

    delete wplikes

--- experiment_apply_learned_bpe_on_other.sh
+++ experiment_apply_learned_bpe_on_other.sh
@@ -4,10 +4,8 @@
 BASE_PATH="data/corpus/experiment_apply_learned_bpe_on_other"
 LMD_ORIG_PATH="data/corpus/lmd_full_nth96_r32_d96_v16_t24_200_16_posevent"
 LMD_DEFAULT_PATH="data/corpus/lmd_full_nth96_r32_d96_v16_t24_200_16_posevent_bpe128_default_ours_1.0"
-LMD_WPLIKE_PATH="data/corpus/lmd_full_nth96_r32_d96_v16_t24_200_16_posevent_bpe128_wplike_ours_1.0"
 SND_ORIG_PATH="data/corpus/snd_nth96_r32_d96_v16_t24_200_16_posevent"
 SND_DEFAULT_PATH="data/corpus/snd_nth96_r32_d96_v16_t24_200_16_posevent_bpe128_default_ours_1.0"
-SND_WPLIKE_PATH="data/corpus/snd_nth96_r32_d96_v16_t24_200_16_posevent_bpe128_wplike_ours_1.0"
 mkdir ${BASE_PATH}
 mkdir "${BASE_PATH}/corpus"
 mkdir "${BASE_PATH}/logs"
@@ -22,13 +20,6 @@ mkdir "${BASE_PATH}/corpus/lmd--snd_default_ours_1.0"
 
 python3 text_to_array.py --bpe 128 --log "${BASE_PATH}/logs/lmd--snd_default_ours_1.0.log" --debug "${BASE_PATH}/corpus/lmd--snd_default_ours_1.0"
 
-mkdir "${BASE_PATH}/corpus/lmd--snd_wplike_ours_1.0"
-./bpe/apply_vocab \
-    $SND_ORIG_PATH \
-    "${BASE_PATH}/corpus/lmd--snd_wplike_ours_1.0/corpus" \
-    "${LMD_WPLIKE_PATH}/shape_vocab" | tee "${BASE_PATH}/logs/lmd--snd_wplike_ours_1.0.log" -a
-
-python3 text_to_array.py --bpe 128 --log "${BASE_PATH}/logs/lmd--snd_wplike_ours_1.0.log" --debug "${BASE_PATH}/corpus/lmd--snd_wplike_ours_1.0"
 
 mkdir "${BASE_PATH}/corpus/snd--lmd_default_ours_1.0"
 ./bpe/apply_vocab \
@@ -38,10 +29,3 @@ mkdir "${BASE_PATH}/corpus/snd--lmd_default_ours_1.0"
 
 python3 text_to_array.py --bpe 128 --log "${BASE_PATH}/logs/snd--lmd_default_ours_1.0.log" --debug "${BASE_PATH}/corpus/snd--lmd_default_ours_1.0"
 
-mkdir "${BASE_PATH}/corpus/snd--lmd_wplike_ours_1.0"
-./bpe/apply_vocab \
-    $LMD_ORIG_PATH \
-    "${BASE_PATH}/corpus/snd--lmd_wplike_ours_1.0/corpus" \
-    "${SND_WPLIKE_PATH}/shape_vocab" | tee "${BASE_PATH}/logs/snd--lmd_wplike_ours_1.0.log" -a
-
-python3 text_to_array.py --bpe 128 --log "${BASE_PATH}/logs/snd--lmd_wplike_ours_1.0.log" --debug "${BASE_PATH}/corpus/snd--lmd_wplike_ours_1.0"
