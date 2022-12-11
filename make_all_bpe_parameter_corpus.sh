c1677b2278f390317f81499b26ce8dbc5fffdd9e
leonw774 <leonw774@gmail.com>
2022-12-11 22:10:06 +0800

    no wplike score from now

--- make_all_bpe_parameter_corpus.sh
+++ make_all_bpe_parameter_corpus.sh
@@ -13,9 +13,3 @@
 # experiment on sample rate
 ./pipeline.sh lmd_full_posevent ours_sample0.1 no_train --use-existed
 ./pipeline.sh snd_posevent ours_sample0.1 no_train --use-existed
-
-# experiment on scoring method
-./pipeline.sh lmd_full_posevent ours_wplike_sample1.0 no_train --use-existed
-./pipeline.sh lmd_full_posevent ours_wplike_sample0.1 no_train --use-existed
-./pipeline.sh snd_posevent ours_wplike_sample1.0 no_train --use-existed
-./pipeline.sh snd_posevent ours_wplike_sample0.1 no_train --use-existed
