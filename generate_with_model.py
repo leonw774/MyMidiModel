6509133bb940c728ab49de8670fd70d4055e87e9
leonw774 <leonw774@gmail.com>
2022-12-22 14:49:12 +0800

    fix typo

--- generate_with_model.py
+++ generate_with_model.py
@@ -148,7 +148,7 @@ def main():
             primer_text_list = primer_text_list[:args.primer_length]
 
         primer_seq = text_list_to_array(primer_text_list, vocabs=model.vocabs)
-        primer_seq = np.expend_dims(primer_seq, axis=0)
+        primer_seq = np.expand_dims(primer_seq, axis=0)
 
     if args.sample_number == 1:
         gen_handler(model, primer_seq, args, args.output_file_path)
