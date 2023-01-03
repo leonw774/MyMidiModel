98195c073f89d8408298bbc4878edb124eed33db
leonw774 <leonw774@gmail.com>
2023-01-04 01:52:04 +0800

    change config of  snd-same-setting

--- make_all_bpe_parameter_corpus.sh
+++ make_all_bpe_parameter_corpus.sh
@@ -15,4 +15,4 @@
 ./pipeline.sh snd_posevent ours_sample0.1 no_train --use-existed
 
 # snd same setting
-./pipeline.sh snd_same_setting ours_sample0.1 no_train --use-existed
+./pipeline.sh snd_same_setting ours_sample1.0 no_train --use-existed
