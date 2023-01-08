213568289369707251503a4f34ce497cb60e067b
leonw774 <leonw774@gmail.com>
2023-01-08 20:17:14 +0800

    update shell script for new name

--- experiment_on_model_and_training.sh
+++ experiment_on_model_and_training.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # basic config
-./pipeline.sh lmd_full_posevent no_bpe linear_small_permute_data --use-existed
-./pipeline.sh snd_posevent      no_bpe linear_small_permute_data --use-existed
+./pipeline.sh lmd_full no_bpe linear_small_permute_data --use-existed
+./pipeline.sh snd      no_bpe linear_small_permute_data --use-existed
 
 # experiments on BPE usage
-./pipeline.sh lmd_full_posevent ours_sample1.0 linear_small_permute_data --use-existed
-./pipeline.sh snd_posevent      ours_sample1.0 linear_small_permute_data --use-existed
+./pipeline.sh lmd_full ours_sample1.0 linear_small_permute_data --use-existed
+./pipeline.sh snd      ours_sample1.0 linear_small_permute_data --use-existed
 
 # experiments on set loss
-./pipeline.sh lmd_full_posevent ours_sample1.0 linear_small_permute_data+loss --use-existed
-./pipeline.sh snd_posevent      ours_sample1.0 linear_small_permute_data+loss --use-existed
+./pipeline.sh lmd_full ours_sample1.0 linear_small_permute_data+loss --use-existed
+./pipeline.sh snd      ours_sample1.0 linear_small_permute_data+loss --use-existed
 
 # experiment on input data design (??)
 # - posevent v.s. posattr
--- make_all_bpe_parameter_corpus.sh
+++ make_all_bpe_parameter_corpus.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # make original corpus first
-./pipeline.sh lmd_full_posevent no_bpe no_train --use-existed
-./pipeline.sh snd_posevent no_bpe no_train --use-existed
+./pipeline.sh lmd_full no_bpe no_train --use-existed
+./pipeline.sh snd      no_bpe no_train --use-existed
 
 # experiment on merge condition
-./pipeline.sh lmd_full_posevent ours_sample1.0 no_train --use-existed
-./pipeline.sh lmd_full_posevent mulpi_sample1.0 no_train --use-existed
-./pipeline.sh snd_posevent ours_sample1.0 no_train --use-existed
-./pipeline.sh snd_posevent mulpi_sample1.0 no_train --use-existed
+./pipeline.sh lmd_full ours_sample1.0 no_train --use-existed
+./pipeline.sh lmd_full mulpi_sample1.0 no_train --use-existed
+./pipeline.sh snd      ours_sample1.0 no_train --use-existed
+./pipeline.sh snd      mulpi_sample1.0 no_train --use-existed
 
 # experiment on sample rate
-./pipeline.sh lmd_full_posevent ours_sample0.1 no_train --use-existed
-./pipeline.sh snd_posevent ours_sample0.1 no_train --use-existed
+./pipeline.sh lmd_full ours_sample0.1 no_train --use-existed
+./pipeline.sh snd      ours_sample0.1 no_train --use-existed
 
 # same setting as symphonynet
 ./pipeline.sh lmd_same_setting ours_sample1.0 no_train --use-existed
