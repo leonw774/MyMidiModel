fix measure number overflow problem by using uint16

util/corpus.py
@@ -140,7 +140,7 @@ def text_list_to_array(text_list: list, vocabs: Vocabs, input_memory: Union[dict
 
     if input_memory is None:
         last_array_len = 0
-        x = np.full((len(text_list), len(COMPLETE_ATTR_NAME)), fill_value=padding, dtype=np.int16)
+        x = np.full((len(text_list), len(COMPLETE_ATTR_NAME)), fill_value=padding, dtype=np.uint16)
         tracks_count = 0
         cur_position_id = 0
         cur_measure_number = 0 # measure_number starts at 1, 0 is padding
@@ -149,7 +149,7 @@ def text_list_to_array(text_list: list, vocabs: Vocabs, input_memory: Union[dict
         cur_position_cursor = 0
     elif isinstance(input_memory, dict):
         last_array_len = input_memory['last_array'].shape[0]
-        x = np.full((last_array_len+len(text_list), len(COMPLETE_ATTR_NAME)), fill_value=padding, dtype=np.int16)
+        x = np.full((last_array_len+len(text_list), len(COMPLETE_ATTR_NAME)), fill_value=padding, dtype=np.uint16)
         x[:last_array_len] = input_memory['last_array']
         tracks_count = input_memory['tracks_count']
         cur_position_id = input_memory['cur_position_id']

util/dataset.py
@@ -311,8 +311,8 @@ class MidiDataset(Dataset):
 
         # checking for bad numbers
         for fname, fidx in TOKEN_ATTR_INDEX.items():
-            if len(fname) > 3:
-                assert np.min(sampled_array) >= 0,\
+            if len(fname) > 3: # not abbr
+                assert np.min(sampled_array[:, fidx]) >= 0,\
                     (f'number in {fname} is below zero\n'
                     f'{get_input_array_format_string(sampled_array, None, self.vocabs)}')
                 if fname == 'measure_numbers':