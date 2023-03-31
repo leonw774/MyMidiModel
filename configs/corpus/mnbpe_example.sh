b222355ba95466ec09210fceb6f4e331bdd9f6f2
leonw774 <leonw774@gmail.com>
2023-03-31 18:48:33 +0800

    add bpe example piece

---
+++ configs/corpus/mnbpe_example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# midi preprocessing
+NTH=32
+MAX_TRACK_NUMBER=32
+MAX_DURATION=32
+VELOCITY_STEP=16
+CONTINUING_NOTE=true
+USE_MERGE_DRUMS=true
+TEMPO_MIN=24
+TEMPO_MAX=200
+TEMPO_STEP=16
+PROCESS_WORKERS=1
+MIDI_DIR_PATH="data/test_midis/multi-note_bpe_example"
+DATA_NAME="mnbpe_example"
+MIDI_TO_CORPUS_VERBOSE=true
