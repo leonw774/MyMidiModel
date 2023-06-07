15cd55ce4ca05ee21b7d8f9b2da91a2148c72a9a
leonw774 <leonw774@gmail.com>
2023-06-07 20:08:10 +0800

    rename configs

--- pipeline.sh
+++ pipeline.sh
@@ -223,7 +223,7 @@ fi
 python3 evaluate_model_wrapper.py \
     --model-dir-path "$model_dir_path" --num-workers $PROCESS_WORKERS --midi-to-piece-paras "$EVAL_MIDI_TO_PIECE_PARAS_FILE" \
     --log-path "$log_path" --softmax-temperature $SOFTMAX_TEMPERATURE --nucleus-sampling-threshold $NUCLEUS_THRESHOLD --seed $SEED -- \
-    "$MIDI_DIR_PATH" $TEST_PATHLIST $PRIMER_LENGTH
+    "$MIDI_DIR_PATH" "$TEST_PATHLIST" $PRIMER_LENGTH
 
 test "$?" -ne 0 && { echo "Evaluation failed. pipeline.sh exit." | tee -a "$log_path" ; } && exit 1
 echo "All done. pipeline.sh exit." | tee -a "$log_path"
