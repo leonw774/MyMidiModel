73f728dbd58c52eb911e3f25fead6b426aa6f455
leonw774 <leonw774@gmail.com>
2023-08-25 12:23:52 +0800

    fix script

--- experiment_full_model_and_ablation.sh
+++ experiment_full_model_and_ablation.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
 # ABLATIONS
-if [ $# -eq 0 ] ; then
+if [ $# -eq 0 ]; then
     echo "need dataset name"
-    exit 0
+    exit 1
 else 
-    if [ $# -eq 1 ] ; then
+    if [ $# -eq 1 ]; then
         dataset_name=$1
     else
-        if [ $# -eq 2 ] ; then
+        if [ $# -eq 2 ]; then
             dataset_name=$1
             do_ablation=$2
-            if [ $do_ablation -ne '--ablation' ] then
+            if [ $do_ablation != '--ablation' ]; then
                 echo "invalid argument"
-                exit 0
+                exit 1
             fi
+        else
+            echo "invalid argument"
+            exit 1
         fi
     fi
 fi
 
+if [ $do_ablation != '--ablation' ]; then
+    # full model
+    ./pipeline.sh $dataset_name ours_sample1.0 linear_small --use-existed
+else
+    # no BPE
+    ./pipeline.sh $dataset_name no_bpe linear_small --use-existed
 
-# full component
-./pipeline.sh $dataset_name ours_sample1.0 linear_small --use-existed
+    # not use MPS order as position number
+    ./pipeline.sh $dataset_name ours_sample1.0 linear_small_no_mps --use-existed
 
-test -z "$do_ablation" && exit 0
-
-# no BPE
-./pipeline.sh $dataset_name no_bpe linear_small --use-existed
-
-# not use MPS order as position number
-./pipeline.sh $dataset_name ours_sample1.0 linear_small_no_mps --use-existed
-
-# no continuative duration encoding
-./pipeline.sh "${dataset_name}_no_contin" ours_sample1.0 linear_small --use-existed
+    # no continuative duration encoding
+    ./pipeline.sh "${dataset_name}_no_contin" ours_sample1.0 linear_small --use-existed
+fi
